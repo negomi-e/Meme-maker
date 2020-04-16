b892889988797473f55cffe0a318ea9939cb5483
Imogen <imogen.eddings@gmail.com>
2020-04-16 09:35:21 +0100

    First bootstrap

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const app = express();
 
 const reqRouter = require('./router/reg');
 const hbs = require('hbs')
+const path = require('path');
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 // HBS PARTIALS AND TEMPLATES
-hbs.registerPartials(`${__dirname}/public/views/partials`)
+hbs.registerPartials(`${__dirname}/views/partials`)
 hbs.registerHelper('template', (templateName) => {
   const data = fs.readFileSync(`./public/views/partials/${templateName}.hbs`, 'utf8');
   return data;
