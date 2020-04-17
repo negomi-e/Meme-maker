ad8924707a25e32e989f5eabdea50b4829086c74
Imogen <imogen.eddings@gmail.com>
2020-04-17 14:47:46 +0100

    img edits

--- router/create.js
+++ router/create.js
@@ -1,7 +1,26 @@
 const router = require('express').Router();
+const {Category, Meme} = require('../models/models')
 
-router.get('/', (req, res) => {
-  res.render('./collection/create');
+router.get('/', async (req, res) => {
+  let mainCat = await Category.find()
+  console.log(mainCat);
+  res.render('./collection/create', {mainCat});
 })
+
+// SAVE THE NEW IMAGE /save?
+// router.post('/', async (req, res)=>{
+//   console.log(req.body);
+  
+//   let newmeme = new Meme({
+//       author: req.session.user.username,
+//       mainCategory: req.body,
+//       subCategory: req.body,
+//       text: req.body.text,
+//       createdAt: new Date()
+//   })
+
+//   // await newmeme.save()
+//   res.redirect('/')
+// })
 
 module.exports = router;
