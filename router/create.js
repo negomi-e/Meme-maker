65d2cfc0953c3f1ca72f8544507debb479f45cf1
Andrey Semenov <semnov.andrey@gmail.com>
2020-04-17 16:44:50 +0300

    add img to homepage

--- router/create.js
+++ router/create.js
@@ -1,7 +1,28 @@
 const router = require('express').Router();
+const { Meme } = require('../models/models');
+const mongoose = require('mongoose');
+const connectionAddress = 'mongodb://localhost:27017/meme';
+mongoose.connect(connectionAddress, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 router.get('/', (req, res) => {
   res.render('./collection/create');
 })
+
+
+//Save meme
+router.post('/save', async (req, res) => {
+  const meme = new Meme({
+    img: req.body.imgData,
+    createdAt: new Date(),
+    author: req.session.user.username,
+  })
+  await meme.save();
+  res.json({
+    success: true,
+  })
+});
+
+
 
 module.exports = router;
